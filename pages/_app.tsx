10068f3935797a93d5b99be7ae8772136dfd6fd6
kishanhitk <kishansharma1231@gmail.com>
2021-07-24 21:22:46 +0530

    ✨ Add fetching projects on home page

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,13 @@
 import { ChakraProvider } from "@chakra-ui/react";
+import "@fontsource/poppins/100.css";
+import "@fontsource/poppins/200.css";
+import "@fontsource/poppins/300.css";
 import "@fontsource/poppins/400.css";
+import "@fontsource/poppins/500.css";
+import "@fontsource/poppins/600.css";
 import "@fontsource/poppins/700.css";
 import theme from "./../theme/index";
+
 function MyApp({ Component, pageProps }) {
   return (
     <ChakraProvider theme={theme}>
---
+++ types/projects.ts
@@ -0,0 +1,12 @@
+export interface Project {
+  id: string;
+  title: string;
+  shortDescription: string;
+  longDescription: string;
+  slug: string;
+  images: string[];
+  souceLink: string;
+  website: string;
+  upvote_count: number;
+  hashtags: string[];
+}
