f2658d905824671ce67b8253a40e019a19deb64a
kishanhitk <kishansharma1231@gmail.com>
2021-08-17 21:20:34 +0530

    feat: Add upvote and downvote functionality

--- components/ProjectDisplayCardWithButton.tsx
+++ components/ProjectDisplayCardWithButton.tsx
@@ -14,6 +14,7 @@ import Link from "next/link";
 import axios from "axios";
 import { BASE_URL } from "config";
 import { useToast } from "@chakra-ui/react";
+import UpvoteButton from "./UpvoteButton";
 interface IProjectDisplayCardProps {
   project: Project;
 }
@@ -22,39 +23,7 @@ function ProjectDisplayCardWithButtons({ project }: IProjectDisplayCardProps) {
   const [upvoteButtonLoading, setUpvoteButtonLoading] = React.useState(false);
   const boxBGcolor = useColorModeValue("gray.100", "gray.700");
 
-  const upvoteProject = async () => {
-    setUpvoteButtonLoading(true);
-    const token = JSON.parse(window.localStorage.getItem("token"));
-    console.log(token);
-    try {
-      const res = await axios.put(
-        `${BASE_URL}/projects/${project.slug}/upvote`,
-        {},
-        {
-          headers: {
-            Authorization: `Bearer ${token}`,
-          },
-        }
-      );
-      console.log(res);
-      toast({
-        title: "Upvoted",
-        status: "success",
-        duration: 5000,
-        isClosable: true,
-      });
-    } catch (error) {
-      console.log(error);
-      toast({
-        title: "Failed",
-        description: error.response.data.message,
-        status: "warning",
-        duration: 5000,
-        isClosable: true,
-      });
-    }
-    setUpvoteButtonLoading(false);
-  };
+
 
   return (
     <Stack my={7} bgColor={boxBGcolor} p={5} rounded={5} spacing={3}>
@@ -89,14 +58,7 @@ function ProjectDisplayCardWithButtons({ project }: IProjectDisplayCardProps) {
           <Button variant="solid" colorScheme="messenger">
             Try it
           </Button>
-          <Button
-            variant="solid"
-            colorScheme="messenger"
-            onClick={upvoteProject}
-            isLoading={upvoteButtonLoading}
-          >
-            Upvote
-          </Button>
+          <UpvoteButton project={project} />
         </HStack>
       </HStack>
       <Text fontSize="lg">{project.shortDescription}</Text>
---
+++ components/UpvoteButton.tsx
@@ -0,0 +1,128 @@
+import { Button, toast, useToast } from "@chakra-ui/react";
+import axios from "axios";
+import { BASE_URL } from "config";
+import React, { useEffect } from "react";
+import { Project } from "types/projects";
+interface IUpvoteButtonProps {
+  project: Project;
+}
+function UpvoteButton({ project }: IUpvoteButtonProps) {
+  const toast = useToast();
+  const [upvoteButtonLoading, setUpvoteButtonLoading] = React.useState(false);
+  const [upvoted, setUpvoted] = React.useState(true);
+
+  const checkIfUpvoted = async () => {
+    setUpvoteButtonLoading(true);
+    const token = JSON.parse(window.localStorage.getItem("token"));
+    const res = await axios.get(`${BASE_URL}/projects/${project.slug}/upvote`, {
+      headers: {
+        Authorization: `Bearer ${token}`,
+      },
+    });
+    console.log(res);
+    if (res.data) {
+      setUpvoted(true);
+    } else {
+      setUpvoted(false);
+    }
+    setUpvoteButtonLoading(false);
+  };
+  const upvoteProject = async () => {
+    setUpvoteButtonLoading(true);
+    const token = JSON.parse(window.localStorage.getItem("token"));
+    console.log(token);
+    try {
+      const res = await axios.put(
+        `${BASE_URL}/projects/${project.slug}/upvote`,
+        {},
+        {
+          headers: {
+            Authorization: `Bearer ${token}`,
+          },
+        }
+      );
+      console.log(res);
+      setUpvoted(true);
+      toast({
+        title: "Upvoted",
+        status: "success",
+        duration: 5000,
+        isClosable: true,
+      });
+    } catch (error) {
+      console.log(error);
+      toast({
+        title: "Failed",
+        description: error.response.data.message,
+        status: "warning",
+        duration: 5000,
+        isClosable: true,
+      });
+    }
+    setUpvoteButtonLoading(false);
+  };
+  const deleteUpvote = async () => {
+    setUpvoteButtonLoading(true);
+    const token = JSON.parse(window.localStorage.getItem("token"));
+    console.log(token);
+    try {
+      const res = await axios.delete(
+        `${BASE_URL}/projects/${project.slug}/upvote`,
+        {
+          headers: {
+            Authorization: `Bearer ${token}`,
+          },
+        }
+      );
+      console.log(res);
+      setUpvoted(false);
+      toast({
+        title: "Removed Upvote!",
+        status: "warning",
+        duration: 5000,
+        isClosable: true,
+      });
+    } catch (error) {
+      console.log(error);
+      toast({
+        title: "Failed",
+        description: error.response.data.message,
+        status: "warning",
+        duration: 5000,
+        isClosable: true,
+      });
+    }
+    setUpvoteButtonLoading(false);
+  };
+
+  useEffect(() => {
+    checkIfUpvoted();
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+
+  return (
+    <>
+      {upvoted ? (
+        <Button
+          variant="outline"
+          colorScheme="red"
+          onClick={deleteUpvote}
+          isLoading={upvoteButtonLoading}
+        >
+          Downvote
+        </Button>
+      ) : (
+        <Button
+          variant="solid"
+          colorScheme="green"
+          onClick={upvoteProject}
+          isLoading={upvoteButtonLoading}
+        >
+          Upvote
+        </Button>
+      )}
+    </>
+  );
+}
+
+export default UpvoteButton;
